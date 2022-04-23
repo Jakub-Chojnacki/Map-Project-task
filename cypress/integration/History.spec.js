a69290533ea6aabf5c57a13a4253e31172687cb2
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2022-04-23 15:16:13 +0200

    wrote key tests - searching route, calculating price and changing price per km

---
+++ cypress/integration/History.spec.js
@@ -0,0 +1,16 @@
+
+describe("Test whether history works", ()=> {
+    it("Shows fallback if history is empty", ()=>{
+        cy.visit("localhost:3000");
+        cy.contains("button", "Show History").click({force:true})
+        cy.contains("p","There is no history to show")
+    })
+    it("Can show history if it's saved", ()=>{
+        cy.visit("localhost:3000");
+        cy.setLocalStorage("searches",'[{"addressA":" London, United Kingdom","addressB":" Manchester, United Kingdom","id":1}]')
+        cy.contains("button", "Show History").click({force:true})
+        cy.contains("div","From London, United Kingdom To Manchester, United Kingdom")
+        
+    })
+
+})
---
+++ cypress/integration/Main.spec.js
@@ -0,0 +1,36 @@
+
+describe("Test homepage", ()=>{
+    it("can input waypointA", ()=>{
+        cy.visit("localhost:3000");
+        cy.get('input')
+        cy.get('input[placeholder="Enter point A"]').type('Gdańsk{enter}')
+        cy.get('li').first().click({force:true})
+        cy.wait(1000)
+    })
+
+    it("can input waypointB", ()=> {  
+        cy.get('input[placeholder="Enter point B"]').type('Sopot{enter}')
+        cy.wait(1000)
+        cy.get('li[data-result-index="0"] a').click({force:true, multiple:true})
+        cy.wait(1000)
+    })
+
+    it("shows results button", ()=> {
+        cy.contains("button", "Check results").click()
+        cy.wait(1000)
+    })
+
+    it("displays the results", ()=> {
+        cy.contains("p","You are traveling from: Gdańsk, Polska")
+        cy.contains("p","To: Sopot, Polska")
+        cy.contains("p", "Price per km: 1.25")
+        cy.contains("p", "Total cost: 1017.06zł")
+    })
+
+    it("Can change price per km", ()=> {
+        cy.get('input').clear().type("1.30")
+        cy.contains("p", "Price per km: 1.30")
+        cy.contains("p","Total cost: 1017.75zł")
+    })
+
+})
